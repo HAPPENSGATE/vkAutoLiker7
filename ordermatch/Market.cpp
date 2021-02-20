00f86014304cc879aeee7cd663b4d024d6727831
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-02-20 10:16:12 +0000

    Release v2.3.1

--- ordermatch/Market.cpp
+++ ordermatch/Market.cpp
@@ -16,3 +16,57 @@
 ** See http://www.quickfixengine.org/LICENSE for licensing information.
 **
 ** Contact ask@quickfixengine.org if any conditions of this licensing are
+** not clear to you.
+**
+****************************************************************************/
+
+#ifdef _MSC_VER
+#pragma warning( disable : 4786 )
+#endif
+
+#include "Market.h"
+#include <iostream>
+
+bool Market::insert( const Order& order )
+{
+  if ( order.getSide() == Order::buy )
+    m_bidOrders.insert( BidOrders::value_type( order.getPrice(), order ) );
+  else
+    m_askOrders.insert( AskOrders::value_type( order.getPrice(), order ) );
+  return true;
+}
+
+void Market::erase( const Order& order )
+{
+  std::string id = order.getClientID();
+  if ( order.getSide() == Order::buy )
+  {
+    BidOrders::iterator i;
+    for ( i = m_bidOrders.begin(); i != m_bidOrders.end(); ++i )
+      if ( i->second.getClientID() == id )
+      {
+        m_bidOrders.erase( i );
+        return ;
+      }
+  }
+  else if ( order.getSide() == Order::sell )
+  {
+    AskOrders::iterator i;
+    for ( i = m_askOrders.begin(); i != m_askOrders.end(); ++i )
+      if ( i->second.getClientID() == id )
+      {
+        m_askOrders.erase( i );
+        return ;
+      }
+  }
+}
+
+bool Market::match( std::queue < Order > & orders )
+{
+  while ( true )
+  {
+    if ( !m_bidOrders.size() || !m_askOrders.size() )
+      return orders.size() != 0;
+
+    BidOrders::iterator iBid = m_bidOrders.begin();
+    AskOrders::iterator iAsk = m_askOrders.begin();
