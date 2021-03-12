45a20a72c91243a89754c27cc3eb6b1d765a7e7c
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-03-12 05:36:13 +0000

    Remove some unused code

--- ordermatch/Market.h
+++ ordermatch/Market.h
@@ -12,4 +12,4 @@
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
-** See http://www.qui
+** See http://www.quickfixengine.org/LICENSE for licensing inf
