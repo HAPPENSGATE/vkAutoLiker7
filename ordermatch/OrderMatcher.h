1bb2f98520a4a1c3c8b04e4c1e30da08a711972e
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-05-03 20:02:02 +0000

    New translations default.json (Acholi)

--- ordermatch/OrderMatcher.h
+++ ordermatch/OrderMatcher.h
@@ -9,4 +9,5 @@
 ** license as defined by quickfixengine.org and appearing in the file
 ** LICENSE included in the packaging of this file.
 **
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INC
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY A
