aeb9b7a57f3b47a1c4f43d5e98e56ff41c6c250c
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-05-08 15:10:13 +0000

    Fixed readme.md

--- ordermatch/OrderMatcher.h
+++ ordermatch/OrderMatcher.h
@@ -12,4 +12,4 @@
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
-** Se
+** See http://www.quickfixengine.org/LICENSE f
