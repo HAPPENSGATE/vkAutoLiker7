394f43b94d0edfb5bcc6d18dc41d826cb6f700df
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-05-08 16:13:38 +0000

    Rename variables.

--- ordermatch/OrderMatcher.h
+++ ordermatch/OrderMatcher.h
@@ -12,4 +12,6 @@
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
-** See http://www.quickfixengine.org/LICENSE f
+** See http://www.quickfixengine.org/LICENSE for licensing information.
+**
+** Contact as
