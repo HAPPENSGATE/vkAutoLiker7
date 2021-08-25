ed8bf72304f37f787403fce145eccf4b1602a5d1
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-08-25 08:55:03 +0000

    version 1.4.5

--- ordermatch/ordermatch.cpp
+++ ordermatch/ordermatch.cpp
@@ -10,4 +10,6 @@
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
-** See http://www.quickfixengine.org/LICENSE for licensing inform
+** See http://www.quickfixengine.org/LICENSE for licensing information.
+**
+** Contact ask@quickfixengi
