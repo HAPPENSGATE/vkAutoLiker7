5bb7d1a2cdb52a389b626ed6aa4136b30ab769f5
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-08-31 07:33:12 +0000

    coveralls integration

--- ordermatch/ordermatch.cpp
+++ ordermatch/ordermatch.cpp
@@ -15,4 +15,6 @@
 ** Contact ask@quickfixengine.org if any conditions of this licensing are
 ** not clear to you.
 **
-********************************************************
+****************************************************************************/
+
+#include <quickfix/config.
