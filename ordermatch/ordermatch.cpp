98e504266e34a885b8ad9db29330a3f0db90eceb
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-09-28 14:00:37 +0000

    Release 2.3.4

--- ordermatch/ordermatch.cpp
+++ ordermatch/ordermatch.cpp
@@ -45,4 +45,7 @@ int main( int argc, char** argv )
     Application application;
     FIX::FileStoreFactory storeFactory( settings );
     FIX::ScreenLogFactory logFactory( settings );
-    FIX::SocketAcceptor acceptor( application, storeFactory, settings, logFa
+    FIX::SocketAcceptor acceptor( application, storeFactory, settings, logFactory );
+
+    acceptor.start();
+    whil
