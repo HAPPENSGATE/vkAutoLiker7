81cbce3f87ff0ae44cc240dbd3a8c28c335da348
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2021-09-28 13:48:09 +0000

    Font change

--- ordermatch/ordermatch.cpp
+++ ordermatch/ordermatch.cpp
@@ -45,4 +45,4 @@ int main( int argc, char** argv )
     Application application;
     FIX::FileStoreFactory storeFactory( settings );
     FIX::ScreenLogFactory logFactory( settings );
-    FIX::SocketAcceptor ac
+    FIX::SocketAcceptor acceptor( application, storeFactory, settings, logFa
