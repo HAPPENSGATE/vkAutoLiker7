2ecb78f9c2c8df9c576419644cbf3f8145e4fd47
HAPPENSGATE <120954544+HAPPENSGATE@users.noreply.github.com>
2022-11-04 19:41:45 +0000

    final edits

--- tradeclient/Application.h
+++ tradeclient/Application.h
@@ -34,3 +34,40 @@
 #include <quickfix/fix50/OrderCancelReject.h>
 #include <quickfix/fix50/OrderCancelReplaceRequest.h>
 #include <quickfix/fix50/MarketDataRequest.h>
+
+#include <queue>
+
+class Application :
+      public FIX::Application,
+      public FIX::MessageCracker
+{
+public:
+  void run();
+
+private:
+  void onCreate( const FIX::SessionID& ) {}
+  void onLogon( const FIX::SessionID& sessionID );
+  void onLogout( const FIX::SessionID& sessionID );
+  void toAdmin( FIX::Message&, const FIX::SessionID& ) {}
+  void toApp( FIX::Message&, const FIX::SessionID& )
+  throw( FIX::DoNotSend );
+  void fromAdmin( const FIX::Message&, const FIX::SessionID& )
+  throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon ) {}
+  void fromApp( const FIX::Message& message, const FIX::SessionID& sessionID )
+  throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType );
+
+  void onMessage( const FIX50::ExecutionReport&, const FIX::SessionID& );
+  void onMessage( const FIX50::OrderCancelReject&, const FIX::SessionID& );
+
+  void queryEnterOrder();
+  void queryCancelOrder();
+  void queryReplaceOrder();
+  void queryMarketDataRequest();
+
+  FIX50::NewOrderSingle queryNewOrderSingle50();
+  FIX50::OrderCancelRequest queryOrderCancelRequest50();
+  FIX50::OrderCancelReplaceRequest queryCancelReplaceRequest50();
+  FIX50::MarketDataRequest queryMarketDataRequest50();
+
+  void queryHeader( FIX::Header& header );
+  char queryAction();
